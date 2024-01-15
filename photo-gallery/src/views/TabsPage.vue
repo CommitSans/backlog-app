753d432ed30fcaeece4b780e08187ac5e4c2902e
ivoriginal <ivo.vilches@gmail.com>
2024-01-15 10:52:41 +0100

    Update TabsPage.vue

--- photo-gallery/src/views/TabsPage.vue
+++ photo-gallery/src/views/TabsPage.vue
@@ -2,17 +2,20 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <!-- <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Backlog</ion-label>
         </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Dice</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>You</ion-label>
         </ion-tab-button>
-      </ion-tab-bar>
+      </ion-tab-bar> -->
     </ion-tabs>
   </ion-page>
 </template>
