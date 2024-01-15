97946fbd4dd6f53c637fec1a29f32e3fcdae2203
ivoriginal <ivo.vilches@gmail.com>
2024-01-15 10:52:38 +0100

    Create Tab2Page.vue

---
+++ photo-gallery/src/views/Tab2Page.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+
+    <!-- -->
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Random</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <!-- / -->
+
+    <ion-content :fullscreen="true">
+      <!--  -->
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Random</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <!-- / -->
+
+      <!-- <BacklogContainer name="Backlog" /> -->
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import BacklogContainer from '@/components/BacklogContainer.vue';
+</script>
