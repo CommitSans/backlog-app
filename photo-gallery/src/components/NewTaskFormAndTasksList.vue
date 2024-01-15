43280b9d946f61467c837d47f15ea16303b816d7
ivoriginal <ivo.vilches@gmail.com>
2024-01-15 10:52:13 +0100

    Create NewTaskFormAndTasksList.vue

---
+++ photo-gallery/src/components/NewTaskFormAndTasksList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-page>
+    <NewTaskForm />
+    <TaskList />
+  </ion-page>
+</template>
+
+<script>
+
+  import NewTaskForm from '../components/NewTaskForm.vue'
+  import TasksList from '../components/TasksList.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      NewTaskForm,
+      TasksList,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
