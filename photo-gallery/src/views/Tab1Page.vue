9961684efdb64c9d672892f9cf4051f837060d76
ivoriginal <ivo.vilches@gmail.com>
2024-01-15 10:52:37 +0100

    Update Tab1Page.vue

--- photo-gallery/src/views/Tab1Page.vue
+++ photo-gallery/src/views/Tab1Page.vue
@@ -6,7 +6,15 @@
       <ion-toolbar>
         <ion-title>Backlog</ion-title>
         <ion-buttons :collapse="true" slot="end">
-          <ion-button>Edit</ion-button>
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="personCircle"></ion-icon>
+          </ion-button>
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="personCircle"></ion-icon>
+          </ion-button>
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="personCircle"></ion-icon>
+          </ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
@@ -18,7 +26,18 @@
         <ion-toolbar>
           <ion-title size="large">Backlog</ion-title>
           <ion-buttons :collapse="true" slot="end">
-            <ion-button>Edit</ion-button>
+            <ion-button>
+              <ion-icon slot="icon-only" :icon="createOutline"></ion-icon>
+            </ion-button>
+            <ion-button>
+              <ion-icon slot="icon-only" :icon="addCircleOutline"></ion-icon>
+            </ion-button>
+            <ion-button>
+              <!-- <ion-icon slot="icon-only" :icon="personCircle"></ion-icon> -->
+              <ion-avatar>
+                <img alt="Silhouette of a person's head" src="https://pbs.twimg.com/profile_images/1661688257938087936/DzSsGyeO_400x400.jpg" />
+              </ion-avatar>
+            </ion-button>
           </ion-buttons>
         </ion-toolbar>
       </ion-header>
@@ -31,5 +50,7 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, createOutline, addCircleOutline, pencilOutline, search, star } from 'ionicons/icons';
 import BacklogContainer from '@/components/BacklogContainer.vue';
 </script>
